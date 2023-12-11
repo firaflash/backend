ff39506e2609783f3a8a91c0c05fff9b2c8deddf
fira flash <flexflash09@gmail.com>
2023-12-11 13:55:05 +0300

    NEW PROJECT

---
+++ config.js
@@ -0,0 +1,4 @@
+export const port = 5555;
+export const mongoDBURL = 'mongodb+srv://firafflash:902178@book-store.e4ndk5o.mongodb.net/bookss?retryWrites=true&w=majority'
+
+//new_data_base
---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { port, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+
+
+const app = express();
+app.get('/', (request, Response) => {
+    console.log(request)
+    return Response.status(234).send('welcome To fira flash website');
+});
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('app connected to data base');
+        app.listen(port, () => {
+            console.log(`app is listening to port: ${port}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+
+
